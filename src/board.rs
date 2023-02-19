896d3b44fb6a22a76b8480db2e42ee57f5550ec1
Samson Main <ib21231@bristol.ac.uk>
2023-02-19 12:57:10 +0000

    create test for move generation

--- src/board.rs
+++ src/board.rs
@@ -2,6 +2,10 @@ struct Board {
 
 }
 
+fn build_board(fen: String) -> Option<Board> {
+    None
+}
+
 fn generate_moves(board: Board) -> Vec<Board> {
     generate_pseudo_legal_moves(board)
         .into_iter()
@@ -22,4 +26,26 @@ fn is_legal(board: &Board) -> bool {
 
 #[cfg(test)]
 mod tests {
+    use crate::board::{Board, build_board};
+
+    #[test]
+    fn perft() {
+        let expected = [1, 20];
+        let mut nodes = Vec::new();
+        for i in &expected {
+            nodes = next_nodes(nodes);
+            assert_eq!(nodes.len(), *i);
+        }
+    }
+
+    fn next_nodes(nodes: Vec<Board>) -> Vec<Board> {
+        match nodes.len() {
+            0 => {
+                let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+                let board = build_board(fen).unwrap();
+                vec![board]
+            },
+            _ => Vec::new()
+        }
+    }
 }
